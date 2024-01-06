b81114d996dccff139fac95c417c650ead8a98d9
ketsar28 <muhammadketsar1@gmail.com>
2024-01-06 22:12:33 +0700

    [FEATURE] login - register - home (page) finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       tablet: "640px",
       // => @media (min-width: 640px) { ... }
 
-      smallTablet: "768px",
+      mediumTablet: "768px",
       // => @media (min-width: 768px) { ... }
       laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
